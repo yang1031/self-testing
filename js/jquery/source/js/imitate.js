2210d4a2021320190531a6f4183b4db6df165426
jiayi <jiayi.yang@bosinlife.com>
2017-10-16 17:54:56 +0800

    后台接口还没写好，只能自己弄点jquery东西了，好难呀，明天如果还是没事干，或许可以先把网站搞起来，用angular2还是vue呢

--- js/jquery/source/js/imitate.js
+++ js/jquery/source/js/imitate.js
@@ -34,9 +34,24 @@ $.fn.myEnd = function () {
 // };
 console.log($(".m-div")[0]);
 console.log($(".m-div").eq(0));
-console.log(setDocument("title"));
 // console.log($(".m-div")[0].myAttr('title', 'aa'));  错误的错误的
 /*
- * line-1554
+ * 研究下Sizzle line-754  该方法主要用于:
+ * 1、对于单一选择器，且是ID、Tag、Class三种类型之一，则直接获取并返回结果
+ * 2、对于支持querySelectorAll方法的浏览器，通过执行querySelectorAll方法获取并返回匹配的DOM元素
+ * 3、除上之外则调用select方法获取并返回匹配的DOM元素
+ *
+ * function Sizzle( selector, context, results, seed ) {}
+ * @param : 对参数的解释
+ * @param selector 选择器字符串
+ * @param context 执行匹配的最初的上下文（即DOM元素集合）。若context没有赋值，则取document。
+ * @param results 已匹配出的部分最终结果。若results没有赋值，则赋予空数组。
+ * @param seed 初始集合
+ *
+ * attr( elem, name)
+ *
  * ownerDocument  返回对应的文档节点
-  *  */
+ * setDocument  line-1108
+ *   根据当前文档设置文档相关的变量，参数element or document，返回current document
+ *   主要任务是测试浏览器对相关函数的支持，从而写出兼容的ID，TAG，CLASS的find和filter函数
+ *  */
